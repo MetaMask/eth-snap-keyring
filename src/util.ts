45060c70d62ef30f350d18c71e9f6c07ec305170
Daniel Rocha <daniel@rocha.io>
2023-03-20 16:11:06 +0100

    chore: add files from proof-of-concept

---
+++ src/util.ts
@@ -0,0 +1,35 @@
+/**
+ * A deferred Promise.
+ *
+ * A deferred Promise is one that can be resolved or rejected independently of
+ * the Promise construction.
+ *
+ * @typedef {object} DeferredPromise
+ * @property {Promise} promise - The Promise that has been deferred.
+ * @property {() => void} resolve - A function that resolves the Promise.
+ * @property {() => void} reject - A function that rejects the Promise.
+ */
+
+export interface DeferredPromise {
+  promise: Promise<any>;
+  resolve?: () => void;
+  reject?: () => void;
+}
+
+/**
+ * Create a defered Promise.
+ *
+ * @returns A deferred Promise.
+ */
+export function deferredPromise(): DeferredPromise {
+  let resolve: DeferredPromise['resolve'];
+  let reject: DeferredPromise['reject'];
+
+  const promise = new Promise<void>(
+    (innerResolve: () => void, innerReject: () => void) => {
+      resolve = innerResolve;
+      reject = innerReject;
+    },
+  );
+  return { promise, resolve, reject };
+}
