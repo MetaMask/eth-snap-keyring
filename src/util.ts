6c75898412469cbe36ad18ba0fb23e55ce0450db
Daniel Rocha <daniel@rocha.io>
2023-03-21 15:03:10 +0100

    chore: document DeferredPromise class

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,12 @@
+/**
+ * A deferred promise can be resolved by a caller different from the one who
+ * created it.
+ *
+ * Example:
+ * - "A" creates a deferred promise "P", adds it to a list, and awaits it
+ * - "B" gets "P" from the list and resolves it
+ * - "A" gets the resolved value
+ */
 export class DeferredPromise<T> {
   promise: Promise<T>;
 
