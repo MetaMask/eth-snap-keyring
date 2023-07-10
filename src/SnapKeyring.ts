a742dd08bc0a197cf877a99245ac8f08590199f3
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-07-10 12:18:00 +0200

    chore: remove trace

--- src/SnapKeyring.ts
+++ src/SnapKeyring.ts
@@ -434,7 +434,7 @@ export class SnapKeyring extends EventEmitter {
     if (sync) {
       await this.#syncAllSnapsAccounts();
     }
-    const tmp = [...this.#addressToAccount.values()].map((account) => {
+    return [...this.#addressToAccount.values()].map((account) => {
       return {
         ...account,
         metadata: {
@@ -443,7 +443,5 @@ export class SnapKeyring extends EventEmitter {
         },
       };
     });
-    console.log('listAccounts', tmp);
-    return tmp;
   }
 }
