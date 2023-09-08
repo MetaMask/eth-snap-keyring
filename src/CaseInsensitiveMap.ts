42ef7724e6a471a7fa8d1d4ab076630424ed71d4
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-09-08 12:33:32 +0200

    chore: document `CaseInsensitiveMap` methods

--- src/CaseInsensitiveMap.ts
+++ src/CaseInsensitiveMap.ts
@@ -1,12 +1,34 @@
+/**
+ * A case-insensitive map that stores key-value pairs.
+ */
 export class CaseInsensitiveMap<T> extends Map<string, T> {
+  /**
+   * Create a new case-insensitive map from a plain object.
+   *
+   * @param obj - An object with entries to initialize the map with.
+   * @returns A new case-insensitive map with all entries from `obj`.
+   */
   static fromObject<T>(obj: Record<string, T>): CaseInsensitiveMap<T> {
     return new CaseInsensitiveMap(Object.entries(obj));
   }
 
+  /**
+   * Return a plain object with all entries from this map.
+   *
+   * @returns A plain object with all entries from this map.
+   */
   toObject(): Record<string, T> {
     return Object.fromEntries(this.entries());
   }
 
+  /**
+   * Return the value associated to the given key, or `undefined` if the key is
+   * not found.
+   *
+   * @param key - The key to get the value for.
+   * @returns The value associated to the given key, or `undefined` if the key
+   * is not found.
+   */
   get(key: string): T | undefined {
     return super.get(key.toLowerCase());
   }
@@ -27,14 +49,37 @@ export class CaseInsensitiveMap<T> extends Map<string, T> {
     return value;
   }
 
+  /**
+   * Check whether the given key is present in the map.
+   *
+   * @param key - The key to check for.
+   * @returns `true` if the key is present in the map, `false` otherwise.
+   */
   has(key: string): boolean {
     return super.has(key.toLowerCase());
   }
 
+  /**
+   * Set the value for the given key. If the key already exists in the map, its
+   * value will be updated.
+   *
+   * The key is converted to lowercase before being stored in the map to ensure
+   * case-insensitivity.
+   *
+   * @param key - The key to set the value for.
+   * @param value - The value to set.
+   * @returns The map instance.
+   */
   set(key: string, value: T): this {
     return super.set(key.toLowerCase(), value);
   }
 
+  /**
+   * Delete the entry for the given key.
+   *
+   * @param key - The key to delete the entry for.
+   * @returns `true` if the entry was present in the map, `false` otherwise.
+   */
   delete(key: string): boolean {
     return super.delete(key.toLowerCase());
   }
